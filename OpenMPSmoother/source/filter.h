8110f22ad0f86581eec40faec02014ab2b52bcc5
ncarey <ncarey90@gmail.com>
2014-02-24 14:44:40 -0500

    initial commit with code scaffold, makefile, headerfile.

---
+++ OpenMPSmoother/source/filter.h
@@ -0,0 +1,14 @@
+#ifndef _h_filter
+#define _h_filter
+
+#include<stdint.h>
+
+int timeval_subtract (struct timeval * result, struct timeval * x, struct timeval * y);
+void serialFilterFirst ( int data_len, unsigned int* input_array, unsigned int* output_array, int filter_len, unsigned int* filter_list );
+void serialDataFirst ( int data_len, unsigned int* input_array, unsigned int* output_array, int filter_len, unsigned int* filter_list );
+void parallelFilterFirst ( int data_len, unsigned int* input_array, unsigned int* output_array, int filter_len, unsigned int* filter_list );
+void parallelDataFirst ( int data_len, unsigned int* input_array, unsigned int* output_array, int filter_len, unsigned int* filter_list );
+void checkData ( unsigned int * serialarray, unsigned int * parallelarray );
+int posix_memalign (void **, size_t, size_t);
+
+#endif
